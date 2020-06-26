c8d249673fabdee8828254d29a24fecbc6442254
Parker Bidigare <parker.bidigare@usda.gov>
2020-06-26 15:45:33 -0400

    added some code with libraries

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const $ = require('jquery');
+const fs = require('fs');
 
 function produceHtml(json) {
-    
+  var pages = json.pages;
+  pages.forEach((ind, val) => {
+    const pageName = val.pageName;
+    var content = [];
+
+    content.push('<!DOCTYPE html>');
+    content.push('<HTML>');
+    content.push('<HEAD>');
+    content.push('<TITLE>' + val.titleName + '</TITLE>');
+
+    var tags = val.tags;
+
+    tags.forEach((indTag, valTag) => {
+      if (valTag.tagType === 'input') {
+        content.push(
+          '<input type="' + valTag.type + '" class="' + valTag.css + '" name='
+        );
+      }
+    });
+  });
 }
 
 module.exports = produceHtml;
